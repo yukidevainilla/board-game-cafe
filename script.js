1b5273a0afa0c1809f3336bff60f3b84099e7fdd
yukidevainilla <lore.dlg4@gmail.com>
2024-07-02 02:37:25 -0600

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const sectionId = event.target.getAttribute('href').substring(1);
+            const section = document.getElementById(sectionId);
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
